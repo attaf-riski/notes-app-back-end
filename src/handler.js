91772783b769aa4f88bb86e1d8ad31ade18fcd88
attaf-riski <atafriski27@gmail.com>
2023-12-17 09:25:52 +0700

    restructure with hapi plugin

--- src/handler.js
+++ src/handler.js
@@ -50,12 +50,15 @@ const getNoteByIdHandler = (request, h) => {
   const note = notes.filter((n) => n.id === id)[0];
 
   if (note !== undefined) {
-    return {
+    const response = h.response({
       status: 'success',
       data: {
         note,
       },
-    };
+    });
+
+    response.code(200);
+    return response;
   }
 
   const response = h.response({
